caeac7f13289c78d6eeb6c4f6cff34c2c8821341
Arinpe <dehtolah@gmail.com>
2021-08-28 13:29:50 +0100

    Resolved sugesstions

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,7 @@
-export default function Quote() {
-  return (
-    <p className="quote">
-      “Common sense is calculation applied to life. ”
-      ― Henri Frederic Amiel
-    </p>
-  );
-}
+const Quote = () => (
+  <p className="quote">
+    “Common sense is calculation applied to life. ”
+    ― Henri Frederic Amiel
+  </p>
+);
+export default Quote;
--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,16 +1,14 @@
 import Navbar from './nvbar';
 
-function Quote() {
-  return (
-    <>
-      <Navbar />
-      <div>
-        <h1> Welcome to our Page</h1>
-        <p> Mathematics is not about numbers, equations, computations, </p>
-        <p>or algorithms: it is about understanding. –William Paul Thurston</p>
-      </div>
-    </>
-  );
-}
+const Quote = () => (
+  <>
+    <Navbar />
+    <div>
+      <h1> Welcome to our Page</h1>
+      <p> Mathematics is not about numbers, equations, computations, </p>
+      <p>or algorithms: it is about understanding. –William Paul Thurston</p>
+    </div>
+  </>
+);
 
 export default Quote;
