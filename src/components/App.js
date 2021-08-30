b306f9b540953ecd82e9138c35fa51f4f18c9beb
Arinpe <dehtolah@gmail.com>
2021-08-30 12:05:24 +0100

    Update Milestone 5

--- src/components/App.js
+++ src/components/App.js
@@ -1,105 +1,26 @@
-/* eslint-disable no-unused-vars */
-import React, { useState, Component, Fragment } from 'react';
-import ButtonPannel from './ButtonPanel';
-import Display from './Display';
-import Button from './Button';
-import calculate from '../logic/calculate';
+import {
+  Route, BrowserRouter as Router, Switch, Link,
+} from 'react-router-dom';
+import Calculator from './Calculator';
+import Home from './Home';
+import Quote from './Quote';
 
-const buttons = [
-  'AC',
-  '+/-',
-  '%',
-  '/',
-  '9',
-  '8',
-  '7',
-  '*',
-  '6',
-  '5',
-  '4',
-  '+',
-  '3',
-  '2',
-  '1',
-  '-',
-  '0',
-  '.',
-  '=',
-];
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      total: null,
-      prev: '0',
-      next: '0',
-      operator: null,
-    };
-  }
-
-  handledisplay = (prev, next, tot, op) => {
-    let ans;
-    if (tot) {
-      ans = tot;
-    } else if (!op) {
-      ans = prev;
-    } else {
-      ans = next;
-    }
-    return ans;
-  };
-
-  handleClick = e => {
-    const {
-      total, next, operator, prev,
-    } = this.state;
-    const res = calculate(
-      {
-        total,
-        next,
-        operator,
-        prev,
-      },
-      e.target.value,
-    );
-
-    this.setState({
-      ...res,
-    });
-  };
-
-  render() {
-    const {
-      prev, next, total, operator,
-    } = this.state;
-    return (
-      <>
-        <div className="App">
-          <div className="Appcalculator">
-            <Display
-              current={this.handledisplay(prev, next, total, operator)}
-            />
-            <ButtonPannel>
-              {buttons.map(button => {
-                const operators = ['+', '-', '*', '/', '%', 'X²'];
-                const isOperator = operators.includes(button);
-                const isAcBtn = button === 'AC';
-                return (
-                  <Button
-                    onClick={this.handleClick}
-                    key={button}
-                    name={button}
-                    operator={isOperator}
-                    acBtn={isAcBtn}
-                  />
-                );
-              })}
-            </ButtonPannel>
-          </div>
+export default function App() {
+  return (
+    <Router>
+      <div className="nav">
+        <h1 className="logo">Math Magicians</h1>
+        <div className="nav-links">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/calculator" className="nav-link">Calculator</Link>
+          <Link to="/quote" className="nav-link">Quote</Link>
         </div>
-      </>
-    );
-  }
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/calculator" component={Calculator} />
+        <Route exact path="/quote" component={Quote} />
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
