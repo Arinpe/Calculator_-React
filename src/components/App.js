f728df713ca2c5c912c604b14fe0105e5cc1698b
Arinpe <dehtolah@gmail.com>
2021-08-31 17:32:43 +0100

    Update readme file

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import {
   Route, BrowserRouter as Router, Switch, Link,
 } from 'react-router-dom';
 import Calculator from './Calculator';
-import Home from './Home';
+import Home from '/Home';
 import Quote from './Quote';
 
 export default function App() {
