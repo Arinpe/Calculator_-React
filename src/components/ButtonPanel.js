4d29d42969f71cb8f40730b77d535a6a3aaa1af0
Arinpe <dehtolah@gmail.com>
2021-09-07 14:21:41 +0100

    Fix compile error

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,43 +1,7 @@
 import PropTypes from 'prop-types';
-import Button from './Button';
-import calculate from '../logic/calculate';
 
-const buttons = [
-  ['AC', '+/-', '%', '/'],
-  ['9', '8', '7', '*'],
-  ['6', '5', '4', '+'],
-  ['3', '2', '1', '-'],
-  ['0', '.', '='],
-];
-
-export default function ButtonPanel({ prev, next, total, operator, updateValue}) { 
-  const   handleClick = e => {
-    const res = calculate({
-      total, next, operator, prev,
-    }, e.target.value);
-   
-    updateValue({
-      ...res,
-    })
-  };
-  return <div className="button_panel">
-    {buttons.map(group => (
-                <div key={group} className="row">
-                  {group.map(button => {
-                    const operators = ['+', '-', '*', '/', '='];
-                    const isOperator = operators.includes(button);
-                    return (
-                      <Button
-                        onClick={handleClick}
-                        key={button}
-                        name={button}
-                        color={isOperator}
-                        wide={button === '0'}
-                      />
-                    );
-                  })}
-                </div>
-              ))}</div>;
+export default function ButtonPanel({ children }) {
+  return <div className="button_panel">{children}</div>;
 }
 
 ButtonPanel.propTypes = {
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,14 +2,13 @@ import React, { Component, useState } from 'react';
 import ButtonPannel from './ButtonPanel';
 import Display from './Display';
 
-
 const Calculator = () => {
-    const [state, setState] = useState({
-      total: null,
-      prev: '0',
-      next: '0',
-      operator: null,
-    });
+  const [state, setState] = useState({
+    total: null,
+    prev: '0',
+    next: '0',
+    operator: null,
+  });
 
   const handledisplay = (prev, next, tot, op) => {
     let ans;
@@ -22,21 +21,21 @@ const Calculator = () => {
     }
     return ans;
   };
-    const {
-      prev, next, total, operator,
-    } = state;
+  const {
+    prev, next, total, operator,
+  } = state;
 
-    const updateValue = (val) => setState(val);
-    return (
-      <>
-        <div className="App">
-          <div className="App__calculator">
-            <Display current={handledisplay(prev, next, total, operator)} />
-            <ButtonPannel prev={prev} next={next} total={total} operator={operator} updateValue={updateValue} />            
-          </div>
+  const updateValue = val => setState(val);
+  return (
+    <>
+      <div className="App">
+        <div className="App__calculator">
+          <Display current={handledisplay(prev, next, total, operator)} />
+          <ButtonPannel prev={prev} next={next} total={total} operator={operator} updateValue={updateValue} />
         </div>
-      </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
