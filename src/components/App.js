81da12bfaff38dbfde4b1eb6c476628cf63f5517
Arinpe <dehtolah@gmail.com>
2021-09-08 16:07:08 +0100

    Remove commented out code

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, Component, Fragment } from 'react';
-// import ButtonPannel from './ButtonPanel';
-// import Display from './Display';
-// import Button from './Button';
-// import calculate from '../logic/calculate';
 import Calculator from './Calculator';
 
 const App = () => (
